316254a31718a0d47a2750843a7b08ecdb8fe408
kei711 <kei.yam.711@gmail.com>
2021-01-01 01:17:59 +0900

    2-5-2

---
+++ src/main/kotlin/value_object/UserName.kt
@@ -0,0 +1,13 @@
+package value_object
+
+data class UserName(val value: String) {
+    companion object {
+        const val MIN_LENGTH = 3
+    }
+
+    init {
+        require(value.length >= MIN_LENGTH) {
+            "ユーザ名は${MIN_LENGTH}文字以上です。"
+        }
+    }
+}
---
+++ src/test/kotlin/value_object/UserNameTest.kt
@@ -0,0 +1,16 @@
+package value_object
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.assertFailsWith
+
+internal class UserNameTest {
+
+    @Test
+    fun test() {
+        assertEquals("abc", UserName("abc").value)
+        val e = assertFailsWith<IllegalArgumentException> { UserName("ab") }
+        assertEquals("ユーザ名は${UserName.MIN_LENGTH}文字以上です。", e.message)
+    }
+}
